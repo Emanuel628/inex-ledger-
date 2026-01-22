d9f476a733489c744070d3307598a062f0556bd5
Emanuel Castro <emanu62893@gmail.com>
2026-01-21 19:34:53 -0500

    Force SSL for Railway Postgres

--- server/db.js
+++ server/db.js
@@ -1,8 +1,11 @@
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
